f7d35ff4fe372206e1a46ae9a31de624895375ce
Akash Omar <omarakash566@gmail.com>
2025-12-28 15:19:02 +0530

    Fix sidebar image display in house price app

--- app.py
+++ app.py
@@ -13,11 +13,12 @@ X = df.iloc[:,:-3]
 y = df.iloc[:,:-1]
 
 st.sidebar.title('🏘️Select House Features')
-st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
+st.sidebar.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
 all_value = []
 for i in X:
   ans = st.sidebar.slider(f'Select {i} value') 
   all_value.append(ans)
 
 st.write(all_value)
+
 
