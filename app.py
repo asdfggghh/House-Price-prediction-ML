b488dc5bc686b11cd057fc6f1d4f31e5b27f642a
Akash Omar <omarakash566@gmail.com>
2025-12-28 15:17:13 +0530

    Fix CSV reading method in app.py

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from sklearn.datasets import fetch_california_housing
 st.title('🏠House Price prediction using ML')
 st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
 
-df = pd.read.csv('house_data.csv')
+df = pd.read_csv('house_data.csv')
 X = df.iloc[:,:-3]
 y = df.iloc[:,:-1]
 
@@ -20,3 +20,4 @@ for i in X:
   all_value.append(ans)
 
 st.write(all_value)
+
