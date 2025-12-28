a10b3d61b83dac346c42c165bf129afc2f81abf0
Akash Omar <omarakash566@gmail.com>
2025-12-28 15:01:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,4 +4,7 @@ from sklearn.preprocessing import StandardScaler
 import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
+
 st.title('🏠House Price prediction using ML')
+st.image('https://img.freepik.com/free-vector/charming-house-with-tree-illustration_1308-176337.jpg?semt=ais_hybrid&w=740&q=80')
+
